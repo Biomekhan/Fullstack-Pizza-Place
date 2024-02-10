b05091f76ab37d242928f24c2767ddd5b63af102
Biomekhan <79610785+Biomekhan@users.noreply.github.com>
2024-02-10 11:03:57 -0700

    Pizza functions added
    Pizza creation working, list of pizzas shown.

--- pizzaFlask.py
+++ pizzaFlask.py
@@ -12,51 +12,8 @@ app = Flask(__name__)
 #if __name__ == '__main__':
 #    app.run(debug=True)
 available_toppings = []
+
 pizza_list = []
-@app.route('/')
-def index():
-    return render_template('index.html', pizzas = pizza_list, toppings=available_toppings)
-
-@app.route('/add_topping', methods=['POST'])
-def add_topping():
-    new_topping = request.form['new_topping']
-    if new_topping not in available_toppings:
-        available_toppings.append(new_topping)
-    return redirect('/')
-
-@app.route('/remove_topping/<topping>')
-def remove_topping(topping):
-    if topping in available_toppings:
-        available_toppings.remove(topping)
-    return redirect('/')
-
-@app.route('/edit_topping/<old_topping>', methods=['POST'])
-def edit_topping(old_topping):
-    new_topping = request.form['edit']
-    if old_topping in available_toppings and new_topping not in available_toppings:
-        index = available_toppings.index(old_topping)
-        available_toppings[index] = new_topping
-    return redirect('/')
-
-@app.route('/add_pizza', methods=['POST'])
-def add_pizza():
-    size = request.form['size']
-    crust_type = request.form['crust_type']
-    toppings = request.form.getlist('toppings')
-    valid_toppings = [topping for topping in toppings if topping in available_toppings]
-    pizza = pizza(size, *valid_toppings)
-    pizza_list.append(pizza)
-    return redirect('/')
-
-@app.route('/remove_pizza/<int:pizza_index>')
-def remove_pizza(pizza_index):
-    if pizza_index < len(pizza_list):
-        pizza_list.pop(pizza_index)
-    return redirect('/')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 class pizza:
     def __init__(self, toppings):
         self.toppings = []
@@ -86,6 +43,55 @@ class pizza:
         print("Toppings: ")
         for topping in self.toppings:
             print(topping + " ")
+@app.route('/')
+def index():
+    return render_template('index.html', pizzas = pizza_list, toppings=available_toppings)
+
+@app.route('/add_topping', methods=['POST'])
+def add_topping():
+    new_topping = request.form['new_topping']
+    if new_topping not in available_toppings:
+        available_toppings.append(new_topping)
+    return redirect('/')
+
+@app.route('/remove_topping/<topping>')
+def remove_topping(topping):
+    if topping in available_toppings:
+        available_toppings.remove(topping)
+    return redirect('/')
+
+@app.route('/edit_topping/<old_topping>', methods=['POST'])
+def edit_topping(old_topping):
+    new_topping = request.form['edit']
+    if old_topping in available_toppings and new_topping not in available_toppings:
+        index = available_toppings.index(old_topping)
+        available_toppings[index] = new_topping
+    return redirect('/')
+
+@app.route('/create_pizza', methods=['POST'])
+def create_pizza():
+    toppings = request.form.getlist('toppings')
+    newPizza = pizza(toppings)
+    pizza_list.append(newPizza)
+    return redirect('/')
+
+@app.route('/edit_pizza/<int:pizza_index>', methods=['POST'])
+def edit_pizza(pizza_index):
+    if pizza_index < len(pizza_list):
+        toppings = request.form.getlist('toppings')
+        pizza_list[pizza_index].toppings = toppings
+    return redirect('/')
+
+@app.route('/remove_pizza/<int:pizza_index>')
+def remove_pizza(pizza_index):
+    if pizza_index < len(pizza_list):
+        del pizza_list[index]
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 
 
