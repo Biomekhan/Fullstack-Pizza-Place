516f9283e56f13788972662f488f6ea2d8ffb0af
Biomekhan <79610785+Biomekhan@users.noreply.github.com>
2024-02-12 11:03:42 -0700

    Finished functionality

--- pizzaFlask.py
+++ pizzaFlask.py
@@ -1,18 +1,8 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-#@app.route("/")
-#def hello():
-#    return "Hello World!"
 
-#@app.route("/about")
-#def about():
-#    return "About page"
-
-#if __name__ == '__main__':
-#    app.run(debug=True)
 available_toppings = []
-
 pizza_list = []
 class pizza:
     def __init__(self, toppings):
@@ -43,13 +33,19 @@ class pizza:
         print("Toppings: ")
         for topping in self.toppings:
             print(topping + " ")
+
 @app.route('/')
 def index():
+    #Pass the list of pizzas and toppings to the html
     return render_template('index.html', pizzas = pizza_list, toppings=available_toppings)
+
 
 @app.route('/add_topping', methods=['POST'])
 def add_topping():
+    #Add new topping, check if field is left blank
     new_topping = request.form['new_topping']
+    if (new_topping == ""):
+        new_topping = "Error: input left empty"
     if new_topping not in available_toppings:
         available_toppings.append(new_topping)
     return redirect('/')
@@ -62,8 +58,12 @@ def remove_topping(topping):
 
 @app.route('/edit_topping/<old_topping>', methods=['POST'])
 def edit_topping(old_topping):
-    new_topping = request.form['edit']
-    if old_topping in available_toppings and new_topping not in available_toppings:
+    #Edit an existing topping
+    new_topping = request.form['new_topping']
+    if (new_topping == ""):
+        new_topping = "Error: input left empty"
+    #Change the topping if its found in the list
+    if old_topping in available_toppings and new_topping not in available_toppings: 
         index = available_toppings.index(old_topping)
         available_toppings[index] = new_topping
     return redirect('/')
@@ -72,20 +72,30 @@ def edit_topping(old_topping):
 def create_pizza():
     toppings = request.form.getlist('toppings')
     newPizza = pizza(toppings)
-    pizza_list.append(newPizza)
+    dupeFlag = 0
+    #Creates a pizza after checking to see if another like it exists
+    for i in pizza_list:
+        if newPizza.toppings == i.toppings:
+            dupeFlag = 1
+    #With no duplicates, its good to be added into the list
+    if dupeFlag == 0:
+        pizza_list.append(newPizza)
     return redirect('/')
 
 @app.route('/edit_pizza/<int:pizza_index>', methods=['POST'])
 def edit_pizza(pizza_index):
-    if pizza_index < len(pizza_list):
+    #Allows changing the pizzas once they're in the list
+    if pizza_index-1 < len(pizza_list):
         toppings = request.form.getlist('toppings')
-        pizza_list[pizza_index].toppings = toppings
+        pizza_list[pizza_index-1].toppings = toppings
     return redirect('/')
 
 @app.route('/remove_pizza/<int:pizza_index>')
 def remove_pizza(pizza_index):
-    if pizza_index < len(pizza_list):
-        del pizza_list[index]
+    #This simply removes a pizza.
+    print(pizza_index)
+    if pizza_index-1 < len(pizza_list):
+        del pizza_list[pizza_index-1]
     return redirect('/')
 
 if __name__ == '__main__':
